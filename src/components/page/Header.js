410b64ab9e772fe58cce43fefaddc2e4ab193fe4
Jim Martin <jim@muuse.live>
2021-11-03 16:26:50 -0400

    header logo styling bug fix

--- src/components/page/Header.js
+++ src/components/page/Header.js
@@ -1,4 +1,3 @@
-
 export default function Header() {
   return (
     <header>
@@ -7,11 +6,15 @@ export default function Header() {
           <img
             alt="logo"
             href="/"
+            className="header-logo"
             src="https://www.callin.com/_next/image?url=%2Fimage-logo-black.svg&amp;w=64&amp;q=75"
             srcSet="https://www.callin.com/_next/image?url=%2Fimage-logo-black.svg&amp;w=32&amp;q=75 1x, https://www.callin.com/_next/image?url=%2Fimage-logo-black.svg&amp;w=64&amp;q=75 2x"
           />
         </a>
-        <a className="header-link" href="https://apps.apple.com/us/app/callin-social-podcasting/id1568011211">
+        <a
+          className="header-link"
+          href="https://apps.apple.com/us/app/callin-social-podcasting/id1568011211"
+        >
           Download
         </a>
       </div>
