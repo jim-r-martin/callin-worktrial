6a9cccee2f975f6f3ad93541c22ae237b2332701
Jim Martin <jim@muuse.live>
2021-11-04 00:36:01 -0400

    user list clean up

--- src/components/show/EpisodesList.js
+++ src/components/show/EpisodesList.js
@@ -3,7 +3,7 @@ import EpisodeItem from "./EpisodeItem";
 export default function EpisodesList({ episodes }) {
   return (
     <div className="episodes-list">
-      <ul className="view-list">
+      <ul className="view-list list-col">
         {episodes.map((episode) => (
           <li className="view-list-item" key={episode.id}>
             <EpisodeItem episode={episode} />
