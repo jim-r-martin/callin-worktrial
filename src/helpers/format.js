6aa42761111adc255e6c632bd916a6ce56f807ba
Jim Martin <jim@muuse.live>
2021-11-04 06:39:05 -0400

    add comments for format helper funcs

--- src/helpers/format.js
+++ src/helpers/format.js
@@ -1,25 +1,47 @@
 import { MONTH_ABREV_LIST } from "./constants";
 
+/**
+ * Returns grammatically corrent subscribers count info string
+ *
+ * @param {number} subscribersCount Amount of show subscribers
+ * @return {string} subscribers count info string
+ */
 function formatSubscribersStr(subscribersCount) {
   if (!subscribersCount) {
     return "No subscribers yet";
-  } else if (subscribersCount === 1) {
+  }
+
+  if (subscribersCount === 1) {
     return "1 subscriber";
   }
 
   return `${subscribersCount} subscribers`;
 }
 
+/**
+ * Returns grammatically correct episode count info string
+ *
+ * @param {number} episodeCount Amount of show episodes
+ * @return {string} episode count info string
+ */
 function formatEpisodesStr(episodeCount) {
   if (!episodeCount) {
     return "No episodes published yet";
-  } else if (episodeCount === 1) {
+  }
+
+  if (episodeCount === 1) {
     return "1 episode";
   }
 
   return `${episodeCount} episodes`;
 }
 
+/**
+ * Convert seconds into formatted date string (ex. Oct 9 2021)
+ *
+ * @param {number} seconds unix timestamp
+ * @return {string} date string - format: [MMM d y]
+ */
 function formatDate(seconds) {
   const dateObj = new Date(seconds * 1000);
   const month = MONTH_ABREV_LIST[dateObj.getMonth()];
@@ -29,6 +51,13 @@ function formatDate(seconds) {
   return `${month} ${date} ${year}`;
 }
 
+/**
+ * Returns runtime seconds represented as formatted hours and mins string
+ * note: if runtime is less than 2 hours only display in temrs of minutes
+ *
+ * @param {number} seconds runtime seconds
+ * @return {string} hours and mins string
+ */
 function formatRunTime(seconds) {
   const secondsInt = parseInt(seconds);
   const totalMins = Math.floor(secondsInt / 60);
